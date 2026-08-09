ec64c5bd2ce29bcc9c6ef24903cfbeddd6cc0ffb
Shreya Navada <shreyanavada26@gmail.com>
2026-08-09 22:17:11 +0530

    Create solution.sql
    Table: Transactions
    
    +---------------+---------+
    | Column Name   | Type    |
    +---------------+---------+
    | id            | int     |
    | country       | varchar |
    | state         | enum    |
    | amount        | int     |
    | trans_date    | date    |
    +---------------+---------+
    id is the primary key of this table.
    The table has information about incoming transactions.
    The state column is an enum of type ["approved", "declined"].
     
    
    Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
    
    Return the result table in any order.
    
    The query result format is in the following example.
    
     
    
    Example 1:
    
    Input: 
    Transactions table:
    +------+---------+----------+--------+------------+
    | id   | country | state    | amount | trans_date |
    +------+---------+----------+--------+------------+
    | 121  | US      | approved | 1000   | 2018-12-18 |
    | 122  | US      | declined | 2000   | 2018-12-19 |
    | 123  | US      | approved | 2000   | 2019-01-01 |
    | 124  | DE      | approved | 2000   | 2019-01-07 |
    +------+---------+----------+--------+------------+
    Output: 
    +----------+---------+-------------+----------------+--------------------+-----------------------+
    | month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
    +----------+---------+-------------+----------------+--------------------+-----------------------+
    | 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
    | 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
    | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
    +----------+---------+-------------+----------------+--------------------+-----------------------+

---
+++ Basic Aggregate Functions/Monthly Transactions I (IMP)/solution.sql
@@ -0,0 +1,15 @@
+-- Write your MySQL query statement below
+select DATE_FORMAT(trans_date, '%Y-%m') as month , country , count(*) as trans_count , 
+sum(case 
+    when state ='approved' then 1
+    else 0
+end) as approved_count  ,
+sum(amount) as trans_total_amount , 
+sum(case
+ when state ='approved' then amount
+ when state ='declined' then 0
+
+end )as approved_total_amount
+
+from Transactions
+group by DATE_FORMAT(trans_date, '%Y-%m')  , country;
